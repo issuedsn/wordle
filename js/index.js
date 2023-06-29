34eed7d55d0db48faffeb1d4c6b39ba72272152e
heritage_722 <designk1m@naver.com>
2023-06-29 22:35:10 +0900

    각종 오류해결 및 JavaScript 로직 완성
    워들(Wordle)게임을 만들었습니다!

--- js/index.js
+++ js/index.js
@@ -1,2 +1,96 @@
-// 태그를 가져오는 방법
-document.getElementById();
+const 정답 = "APPLE";
+
+let index = 0; //인덱스 값을 수정해야하므로 let 선언
+let attempts = 0; //시도 횟수
+let timer;
+
+function appStart() {
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; font-size:20px; position:absolute; top:40vh; left:41vw; background:#000; opacity:0.5; color:#fff; width:300px; height:70px; border-radius:40px;";
+    document.body.appendChild(div);
+  };
+
+  const gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts += 1;
+    index = 0;
+  };
+
+  const handleEnterKey = () => {
+    let 맞은_갯수 = 0;
+
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const 입력한_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력한_글자 === 정답_글자) {
+        맞은_갯수 += 1;
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력한_글자)) block.style.background = "#C9B458";
+      else block.style.background = "#787c7e";
+
+      block.style.color = "white";
+    }
+
+    if (맞은_갯수 === 5) gameover();
+    else nextLine();
+  };
+
+  const handleBackspalce = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  const handleKeydown = (event) => {
+    const key = event.key.toUpperCase(); // key값 . 대문자선언
+    const keyCode = event.keyCode; //keyCode의 값
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    if (event.key === "Backspace") handleBackspalce();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index += 1; //[다음 블럭으로 입력], 다른 표시로 index = index + 1; && index++;로도 표시 가능, 완전히 같진 않지만 표현 가능.
+    }
+  };
+
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
+  window.addEventListener("keydown", handleKeydown);
+}
+
+appStart();
